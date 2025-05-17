6b720cc8594b60f0ee04c11ac167870e3aa74d2d
504-00\teacher <class-teacher270@edu.oca.ac.jp>
2025-05-17 18:49:57 +0900

    エネミーBが死ぬように。

--- Assets/Scripts/gitTest.cs
+++ Assets/Scripts/gitTest.cs
@@ -5,7 +5,7 @@ public class gitTest : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var a = "";
+        var a = "エネミーBが死ぬ！";
     }
 
     // Update is called once per frame
